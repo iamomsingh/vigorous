954bf63751f9a8f7f23085a0cd742c898a866da7
om singh <iamomsingh11@gmail.com>
2023-12-08 01:03:49 +0530

    mongodb connected to backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("server setup");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello backend user");
+});
+
+// app.use("/api/users", userRoutes);
+
+app.listen(port, () => console.log(`server running on port ${port}`));
